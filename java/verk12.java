c6fce82a01abeba1cd84c627f7af6e505ca808c7
stefan <stefanborgar@gmail.com>
2024-11-17 18:47:12 +0000

    verk12

--- java/verk12.java
+++ java/verk12.java
@@ -1,9 +1,19 @@
 import java.util.Arrays;
 public class verk12{
   public static void main(String[] args) {
-  int[] inntak = {Integer.parseInt(args[0]),Integer.parseInt(args[1])};
+  int[] inntak = {Integer.parseInt(args[0]),Integer.parseInt(args[1]),Integer.parseInt(args[2])};
   System.out.println(naerTiu(inntak[0],inntak[1]));
-  System.out.println(runa(inntak[0],inntak[1]));
+  int[] f = runa(inntak[0],inntak[1]);
+  for (int i = 0; i < f.length; i++) {
+    System.out.print(f[i]+" "); 
+  }
+  System.out.println();
+  System.out.println(sidastiSami(inntak[0],inntak[1]));
+  int[] B = fyllir(inntak);
+  for (int j = 0; j < B.length; j++) {
+   System.out.print(B[j]+" ");
+  }
+  System.out.println();
   }
   public static int naerTiu(int tala1, int tala2){
     int[] tala = {tala1, tala2};
@@ -21,7 +31,7 @@ public class verk12{
       return tala[1];
     }else return tala[0];
     }
-  public static String runa(int tala1, int tala2) {
+  public static int[] runa(int tala1, int tala2) {
 
     
     if (tala1<tala2) {
@@ -29,13 +39,28 @@ public class verk12{
      for (int i = 0; i <= tala2-tala1; i++) {
        f[i]=tala1+i;
      }
-     return "fallid f inniheldur nu " + Arrays.toString(f);
+     return f;
     } else {
-      return"tala 1 er staerri en tala 2";
+      int[] f = {0};
+      return f;
     }
 
   }  
-  
-  
-  }
+  public static boolean sidastiSami(int tala1, int tala2) {
+    if((tala1 % 10)==(tala2%10)){
+      return true;
+    }else return false;
+ } 
+  public static int[] fyllir(int[] arr){
+    int num1 = arr[0];
+    int num2 = arr[2];
+    if (num1 >= num2) {
+      int[] nums={num1,num1,num1};
+      return nums;
+     }else{
+      int[] nums={num2,num2,num2};
+      return nums;
+      }
+  }  
+}
 
