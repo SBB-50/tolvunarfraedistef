2379ba28a1560ff137b467569d5aa7e6709f4762
stefan <stefanborgar@gmail.com>
2025-01-08 21:30:06 +0000

    verk13 fr

--- java/verk13.java
+++ java/verk13.java
@@ -5,7 +5,7 @@ public static void main(String[] args){
   for (int i = 0; i < hlidar.length; i++) {
    System.out.println("hlid " + (i+1) + "kom upp " + hlidar[i] +"sinnum");
   }
-  int[] summur = hluti2(1000);
+  int[] summur = hluti3(1000);
   for (int i = 0; i < hlidar.length; i++) {
    System.out.println("hlid " + (i+1) + "kom upp " + hlidar[i] +"sinnum");
   }
@@ -14,7 +14,7 @@ public static void main(String[] args){
 public static void hluti1() {
   //hluti 1
   System.out.println("1 teningur: "+ kastatening(6));
-  System.out.println("2 teningar: "+ tening(6,2));
+  System.out.println("2 teningar: "+ teningar(6,2));
   System.out.println("12 hlida: "+ kastatening(12));
 }
 public static int kastatening(int fjoldihlida){
@@ -22,12 +22,42 @@ public static int kastatening(int fjoldihlida){
  return tening;
 }
 public static int teningar(int fjoldihlida, int fjolditeninga){
+  int tening = 0;
   for (int j = 0; j < fjolditeninga; j++) {
-    int tening = (int) (Math.random()*(fjoldihlida+1));
+    tening = (int) (Math.random()*(fjoldihlida+1));
   }
   return tening;
 }
 public static int[]hluti2(int fjoldikasta){
+    
+    int[] geymsla = new int[fjoldikasta];
+    int[] teljarafylki2 = new int[1000];
+    int teljari2 = 0;
+      for (int n = 0; n < 1000;n++) {
+        int thusund2 = (int)(Math.random()*13); 
+        teljarafylki2[n]=thusund2;
+        //System.out.println(Arrays.toString(teljarafylki));
+        //System.out.println(thusund);
+        //if(thusund == 6 ){
+      
+        //teljari++;
+      //}
+    }
+    for (int v = 1; v <= 12; v++){
+      //System.out.println(v);
+     for (int s = 0; s < fjoldikasta ; s++) {
+       //System.out.println(s);
+       if(teljarafylki2[s]==v){
+         teljari2++;
+       //  System.out.println(teljari2);
+        } 
+      }
+    geymsla[v] = teljari2;
+    teljari2=0;
+     }
+
+}
+public static int[]hluti3(int fjoldikasta){
     
     int[] geymsla = new int[fjoldikasta];
     int[] teljarafylki2 = new int[1000];
